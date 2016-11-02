f5f9f68916578ae76979b451088b30d5d6bd5694
smadha <msharan@usc.edu>
2016-11-02 15:04:11 -0700

    Final results

--- as4/combining_all.py
+++ as4/combining_all.py
@@ -15,7 +15,7 @@ moms = [0.99]
 start_time = time.time()
 testmodels(X_tr, y_tr, X_te, y_te, arch, actfn='relu', last_act='softmax', reg_coeffs = l2_reg_coeffs,
                 num_epoch = 50, batch_size = 1000, sgd_lr = 1e-5, sgd_decays = decays, sgd_moms = moms,
-                    sgd_Nesterov = True, EStop = False, verbose = 0)
+                    sgd_Nesterov = True, EStop = True, verbose = 0)
 end_time = time.time()
 
 print "Combining previous analysis ends in {0:.3f} seconds".format(end_time-start_time)
